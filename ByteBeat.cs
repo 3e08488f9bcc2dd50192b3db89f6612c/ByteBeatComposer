2562c964c4e658cc8cb2b3b22ec6ef5f5b0ac4ac
Paranormal Activity <69107484+3e08488f9bcc2dd50192b3db89f6612c@users.noreply.github.com>
2023-02-25 14:42:30 -0300

    Add files via upload

---
+++ ByteBeat.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace ByteBeatinterpreter
+{
+	public class ByteBeat
+	{
+		/// Private-member variables.
+		private bool isRunning;
+		private readonly byte[] _data;
+
+		/// Private-member functions
+		private void Load(int channel, int rate, int bps)
+		{
+			/// Credits to https://gist.github.com/dharmatech/6cb8fb83e46d56a1cc37f32e42559714
+			using (MemoryStream stream = new())
+			{
+				this.isRunning = true;
+				var writer = new BinaryWriter(stream);
+				writer.Write("RIFF".ToCharArray());
+				writer.Write((UInt32)0);             // chunk size
+				writer.Write("WAVE".ToCharArray());
+				writer.Write("fmt ".ToCharArray());
+				writer.Write((UInt32)16);            // chunk size
+				writer.Write((UInt16)1);             // audio format
+				writer.Write((UInt16)channel);
+				writer.Write((UInt32)rate);
+				writer.Write((UInt32)(rate * channel * bps / 8)); // byte rate
+				writer.Write((UInt16)(channel * bps / 8));               // block align
+				writer.Write((UInt16)bps);
+				writer.Write("data".ToCharArray());
+				writer.Write((UInt32)(_data.Length * channel * bps / 8));
+				foreach (var elt in _data) writer.Write(elt);
+				writer.Seek(4, SeekOrigin.Begin);                     // seek to header chunk size field
+				writer.Write((UInt32)(writer.BaseStream.Length - 8)); // chunk size
+				stream.Seek(0, SeekOrigin.Begin);
+				try
+				{
+                    new SoundPlayer(stream).PlaySync();
+				}
+				catch (Exception)
+				{
+					this.Stop(stream);
+				}
+			}
+		}
+
+		private void Stop(MemoryStream stream)
+		{
+			if (stream != null && this.isRunning)
+			{
+				this.isRunning = false;
+				stream.Dispose();
+			}
+			return;
+		}
+
+		/// Public-member functions
+		public ByteBeat(byte[] data) => this._data = data;
+
+		public void CreateByteBeat(int channelID=1, int rate=8000, int bits_per_sample=8)
+		{
+			if(!this.isRunning) this.Load(channelID, rate, bits_per_sample);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace ByteBeatinterpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new byte[12000 * 10];
+            for (var t = 0; t < data.Length; t++) data[t] = (byte)(t * (42 & t >> 10));
+            ByteBeat r1 = new(data);
+            r1.CreateByteBeat();
+        }
+    }
+}
