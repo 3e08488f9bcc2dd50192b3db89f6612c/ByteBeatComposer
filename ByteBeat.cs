3b4bef28f4616c970a4b6d5f08c07494ac88ceb9
Paranormal Activity <69107484+3e08488f9bcc2dd50192b3db89f6612c@users.noreply.github.com>
2023-02-27 15:09:30 -0300

    Removed unused casts.

--- ByteBeat.cs
+++ ByteBeat.cs
@@ -9,40 +9,38 @@ namespace ByteBeatinterpreter
 		/// Private-member variables.
 		private bool isRunning;
 		private readonly byte[] _data;
+		private readonly MemoryStream stream;
 
 		/// Private-member functions
 		private void Load(int channel, int rate, int bps)
 		{
 			/// Credits to https://gist.github.com/dharmatech/6cb8fb83e46d56a1cc37f32e42559714
-			using (MemoryStream stream = new())
+			this.isRunning = true;
+			var writer = new BinaryWriter(stream);
+			writer.Write("RIFF".ToCharArray());
+			writer.Write(0);											// chunk size
+			writer.Write("WAVE".ToCharArray());
+			writer.Write("fmt ".ToCharArray());
+			writer.Write(16);											// chunk size
+			writer.Write((UInt16)1);									// audio format
+			writer.Write((UInt16)channel);
+			writer.Write(rate);
+			writer.Write(rate * channel * bps / 8);						// byte rate
+			writer.Write((UInt16)(channel * bps / 8));					// block align
+			writer.Write((UInt16)bps);
+			writer.Write("data".ToCharArray());
+			writer.Write(_data.Length * channel * bps / 8);
+			foreach (var elt in _data) writer.Write(elt);
+			writer.Seek(4, SeekOrigin.Begin);							// seek to header chunk size field
+			writer.Write((UInt32)(writer.BaseStream.Length - 8));		// chunk size
+			stream.Seek(0, SeekOrigin.Begin);
+			try
 			{
-				this.isRunning = true;
-				var writer = new BinaryWriter(stream);
-				writer.Write("RIFF".ToCharArray());
-				writer.Write((UInt32)0);             // chunk size
-				writer.Write("WAVE".ToCharArray());
-				writer.Write("fmt ".ToCharArray());
-				writer.Write((UInt32)16);            // chunk size
-				writer.Write((UInt16)1);             // audio format
-				writer.Write((UInt16)channel);
-				writer.Write((UInt32)rate);
-				writer.Write((UInt32)(rate * channel * bps / 8)); // byte rate
-				writer.Write((UInt16)(channel * bps / 8));               // block align
-				writer.Write((UInt16)bps);
-				writer.Write("data".ToCharArray());
-				writer.Write((UInt32)(_data.Length * channel * bps / 8));
-				foreach (var elt in _data) writer.Write(elt);
-				writer.Seek(4, SeekOrigin.Begin);                     // seek to header chunk size field
-				writer.Write((UInt32)(writer.BaseStream.Length - 8)); // chunk size
-				stream.Seek(0, SeekOrigin.Begin);
-				try
-				{
-                    new SoundPlayer(stream).PlaySync();
-				}
-				catch (Exception)
-				{
-					this.Stop(stream);
-				}
+                new SoundPlayer(stream).PlaySync();
+			}
+			catch (Exception)
+			{
+				this.Stop(stream);
 			}
 		}
 
@@ -53,15 +51,23 @@ namespace ByteBeatinterpreter
 				this.isRunning = false;
 				stream.Dispose();
 			}
-			return;
 		}
 
 		/// Public-member functions
-		public ByteBeat(byte[] data) => this._data = data;
+		public ByteBeat(byte[] data)
+        {
+			this._data = data;
+			this.stream = new MemoryStream();
+		}
 
 		public void CreateByteBeat(int channelID=1, int rate=8000, int bits_per_sample=8)
 		{
 			if(!this.isRunning) this.Load(channelID, rate, bits_per_sample);
 		}
+
+		public void Stop()
+        {
+			this.Stop(this.stream);
+        }
 	}
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# Introduction
+A small program that composes a bytebeat. I was lazy and forgot about this.
