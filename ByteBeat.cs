88d521334457d245c66d09e7661d3588d22783f4
Paranormal Activity <69107484+3e08488f9bcc2dd50192b3db89f6612c@users.noreply.github.com>
2023-04-08 03:36:13 -0300

    fixed tabs

--- ByteBeat.cs
+++ ByteBeat.cs
@@ -18,25 +18,25 @@ namespace ByteBeatinterpreter
 			this.isRunning = true;
 			var writer = new BinaryWriter(stream);
 			writer.Write("RIFF".ToCharArray());
-			writer.Write(0);											// chunk size
+			writer.Write(0);					// chunk size
 			writer.Write("WAVE".ToCharArray());
 			writer.Write("fmt ".ToCharArray());
-			writer.Write(16);											// chunk size
-			writer.Write((UInt16)1);									// audio format
+			writer.Write(16);					// chunk size
+			writer.Write((UInt16)1);				// audio format
 			writer.Write((UInt16)channel);
 			writer.Write(rate);
-			writer.Write(rate * channel * bps / 8);						// byte rate
-			writer.Write((UInt16)(channel * bps / 8));					// block align
+			writer.Write(rate * channel * bps / 8);			// byte rate
+			writer.Write((UInt16)(channel * bps / 8));		// block align
 			writer.Write((UInt16)bps);
 			writer.Write("data".ToCharArray());
 			writer.Write(_data.Length * channel * bps / 8);
 			foreach (var elt in _data) writer.Write(elt);
-			writer.Seek(4, SeekOrigin.Begin);							// seek to header chunk size field
-			writer.Write((UInt32)(writer.BaseStream.Length - 8));		// chunk size
+			writer.Seek(4, SeekOrigin.Begin);			// seek to header chunk size field
+			writer.Write((UInt32)(writer.BaseStream.Length - 8));	// chunk size
 			stream.Seek(0, SeekOrigin.Begin);
 			try
 			{
-                new SoundPlayer(stream).PlaySync();
+                		new SoundPlayer(stream).PlaySync();
 			}
 			catch (Exception)
 			{
@@ -55,7 +55,7 @@ namespace ByteBeatinterpreter
 
 		/// Public-member functions
 		public ByteBeat(byte[] data)
-        {
+        	{
 			this._data = data;
 			this.stream = new MemoryStream();
 		}
@@ -66,8 +66,8 @@ namespace ByteBeatinterpreter
 		}
 
 		public void Stop()
-        {
+		{
 			this.Stop(this.stream);
-        }
+        	}
 	}
 }
